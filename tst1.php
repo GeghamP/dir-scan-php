a5fc90ece4c98f58a2c49e370d984084546b2b72
GeghamP <pgegh15@gmail.com>
2017-07-28 21:14:15 +0400

    Add files via upload

---
+++ tst1.php
@@ -0,0 +1,19 @@
+﻿<?php
+defined("MY_DIR") or define("MY_DIR","/");
+function check($cur=__DIR__){
+	$arr=scandir($cur);
+	$arr=array_splice($arr,2);
+	echo "<ul>";
+	for($i=0;$i<count($arr);$i++){
+		echo "<li><a href='#'>".basename($arr[$i])."<br>";
+		if(is_dir($cur.MY_DIR.$arr[$i])){
+			check ($cur.MY_DIR.$arr[$i]);
+		}
+		echo "</a></li>";
+	}
+	echo "</ul>";
+}
+check();
+
+?>
+
